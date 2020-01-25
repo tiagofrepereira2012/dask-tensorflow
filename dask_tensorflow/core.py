7a4666521b015eb2b852bfe4788a0f44fe03c7c4
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-01-25 16:22:16 +0100

    Trying to workout dask and tensorflow

--- dask_tensorflow/core.py
+++ dask_tensorflow/core.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 
 def start_and_attach_server(spec, job_name=None, task_index=None, dask_worker=None):
-    server = tf.train.Server(spec, job_name=job_name, task_index=task_index)
+    server = tf.distribute.Server(spec, job_name=job_name, task_index=task_index)
     dask_worker.tensorflow_server = server
     dask_worker.tensorflow_queue = Queue()
     return 'OK'
